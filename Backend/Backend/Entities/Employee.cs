ecd502d0eed3dbc2822ee29e9aa64471551da782
brunosilvalamelas <8210103@estg.ipp.pt>
2025-10-31 13:21:38 +0000

    Revert "Autenticação, crud utentes, crud tutores "

--- Backend/Backend/Entities/Employee.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Backend.Entities;
-
-/// <summary>
-/// Represents an employee within the system.
-/// Inherits common personal information from <see cref="User"/>.
-/// </summary>
-public class Employee : User
-{
-}
